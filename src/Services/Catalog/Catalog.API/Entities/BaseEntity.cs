495e6ba24ed9488ad4e3207cfcf065475bd1dd40
Dibakor Roy <dibakorroy56@gmail.com>
2025-11-18 00:11:59 +0600

    Creare Domain

---
+++ src/Services/Catalog/Catalog.API/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities;
+
+public class BaseEntity
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public int IsActive { get; set; } 
+}
