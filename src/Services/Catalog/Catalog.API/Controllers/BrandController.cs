ef50b86240d194cf5107ee97fdaf9c4e5586cf63
Dibakor Roy <dibakorroy56@gmail.com>
2025-12-06 23:55:28 +0600

    Update Ocelot and AllowAnonymous

--- src/Services/Catalog/Catalog.API/Controllers/BrandController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/BrandController.cs
@@ -51,6 +51,7 @@ public class BrandController : ControllerBase
         return Ok(dto);
     }
 
+    [AllowAnonymous]
     [HttpGet("by-category/{categoryId}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
